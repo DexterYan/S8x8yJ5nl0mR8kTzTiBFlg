bf12db08fc6ce9bcdcfc91198502145ad5fd4013
Dexter Yan <yanshaocong@gmail.com>
2014-12-14 18:14:19 +0800

    add crawler to get the xe.com rate

---
+++ crawler.js
@@ -0,0 +1,43 @@
+
+function Crawler (from,to) {
+	this.cheerio = require('cheerio');
+	this.request = require('request');
+	this.mongoose = require('mongoose');
+	this.DataModel = require('./model');
+	this.url = "http://www.xe.com/currencyconverter/convert/?Amount=1&From=" + from + "&To=" + to + "#converter";
+}
+
+Crawler.prototype.getRate = function(callback) {
+	var self = this;
+	self.request(self.url, function(err, res,body){
+		if(err){
+			console.log(err);
+		}else{
+			$ = self.cheerio.load(body);
+			callback(Number($('.uccRes .rightCol').text().match(/(\d[\d\.\*]*)/g)[0]).toFixed(2));
+		}
+	});
+};
+
+Crawler.prototype.saveDb = function(from, to, rate,callback) {
+	var self = this;
+	self.mongoose.connect('mongodb://aftership:1234@ds063170.mongolab.com:63170/aftership');
+	var dataModel = new self.DataModel({
+		from: from,
+		to: to,
+		created_at: new Date(),
+		rate: rate
+	})
+	dataModel.save(function(err,res){
+		if(err){
+			console.log(err);
+			callback(false);
+		}else{
+			console.log(res);
+			callback(true);
+		}
+	})
+};
+
+module.exports = Crawler;
+
